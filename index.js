51e0cc10659abed0736eedea1697e169bbb3e2b9
21miusie <mmengesha27@gmail.com>
2024-12-15 13:03:39 +0300

    show the random button using animation

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ $('document').keypress(function(event) {
     let random_number = Math.floor(Math.random() * 4);
     alert(random_number);
 })
+const sequence = [];
+let random_number = Math.floor(Math.random() * 4);
+sequence.push(random_number);
+let level = 1;
+
+$(document).on('keypress', function() {
+    $('h1').text('Level ' + level + '\n');
+})
+
+$('.button' + random_number).animate({ opacity: 0.4 }, 200).animate({ opacity: 1 }, 200)
 
 $('button').on("click", function() {
-    let random_number = Math.floor(Math.random() * 4);
-    alert(random_number);
+    let button_index = $(this).attr('class');
+    alert(random_number + button_index);
+
+    if (random_number == button_index.slice(-1)) {
+        $('h1').text('Level ' + level++ + '\n');
+    } else {
+        $('h1').text('Game Over😥');
+    }
 })
