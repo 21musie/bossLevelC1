86d8d95a6508b52438e0fe59941e836e4dcf38c1
21miusie <mmengesha27@gmail.com>
2024-12-16 23:13:42 +0300

    right before the final phase

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ $('document').keypress(function(event) {
     alert(random_number);
 })
 const sequence = [];
+const userGuess = 0;
 let random_number = Math.floor(Math.random() * 4);
 sequence.push(random_number);
 let level = 1;
@@ -13,13 +14,27 @@ $(document).on('keypress', function() {
 
 $('.button' + random_number).animate({ opacity: 0.4 }, 200).animate({ opacity: 1 }, 200)
 
-$('button').on("click", function() {
-    let button_index = $(this).attr('class');
-    alert(random_number + button_index);
+checkSequence();
 
-    if (random_number == button_index.slice(-1)) {
-        $('h1').text('Level ' + level++ + '\n');
-    } else {
-        $('h1').text('Game Over😥');
+function showNext() {
+    random_number = Math.floor(Math.random() * 4);
+    sequence.push(random_number);
+    $('.button' + sequence[sequence.length - 1]).animate({ opacity: 0.4 }, 200).animate({ opacity: 1 }, 200)
+}
+
+function checkSequence() {
+
+    for (let i = 0; i < sequence.length;) {
+        $('.button' + sequence[i]).on("click", function() {
+            let button_index = $(this).attr('class');
+            alert(random_number + button_index);
+            if (sequence[i] == button_index.slice(-1)) {
+                i++;
+            } else {
+                $('h1').text('Game Over😥');
+            }
+        })
+
     }
-})
+    showNext();
+}
