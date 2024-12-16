a7444dcb7edc46ed30669f49916b8797146d94a3
21miusie <mmengesha27@gmail.com>
2024-12-17 01:53:20 +0300

    added test node day 18 directory

---
+++ node_d18/testNode.js
@@ -0,0 +1,13 @@
+// const fs = require("fs");
+
+// fs.copyFileSync("1st.txt", "2nd.txt");
+
+// var sp = require("superheroes");
+// console.log(sp.randomSuperhero());
+
+// import superheroes, { randomSuperhero } from 'superheroes';
+
+// console.log(randomSuperhero());
+
+import { randomSuperhero } from 'superheroes';
+console.log(randomSuperhero());
