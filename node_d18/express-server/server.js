d5c17a15913e27da8da1e1fa0bee52cdb05ed173
21miusie <mmengesha27@gmail.com>
2024-12-19 23:23:31 +0300

    server.js update

--- node_d18/express-server/server.js
+++ node_d18/express-server/server.js
@@ -1,13 +1,3 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello mars!</h1>')
-});
-app.get('/contact', (req, res) => {
-    res.send('<h1>Contact marsssss!</h1>')
-});
-app.listen(port, () => {
-    console.log(`app started on port:  ${port}`)
-});
