0408cbb9859cf6882300f56f941a4f4d9cdb9779
21miusie <mmengesha27@gmail.com>
2024-12-27 23:53:08 +0300

    checking weather api with the backup

--- api_weather/backup.js
+++ api_weather/backup.js
@@ -6,11 +6,28 @@ const { CLIENT_RENEG_WINDOW } = require("tls");
 
 const app = express();
 
-});
+app.get("/", (req, res) => {
+    const url = "https://api.openweathermap.org/data/2.5/weather?lat=38.8&lon=8.9&appid=4c06d116afad1db786ea165c4cbe82af&units=metric";
+    https.get(url, (response) => {
+        console.log(response.statusCode);
+        response.on("data", (data) => {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const desc = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
 
-//apparently we cant have more than one res.send statements --error code:'ERR_HTTP_HEADERS_SENT'
-//but we can have multiple res,write statements and then finally call res.send
-// res.send("up and runnig");
+            console.log(icon);
+
+            res.write('<h1>The temprature in your area is ' + temp + ' celcius</br></h1>');
+            res.write(' <h1> and the weather is : ' + desc + '</h1>');
+            res.write("<img src='http://openweathermap.org/img/wn/" + icon + "@2x.png'>");
+            res.send();
+        });
+    });
+
+    //apparently we cant have more than one res.send statements --error code:'ERR_HTTP_HEADERS_SENT'
+    //but we can have multiple res,write statements and then finally call res.send
+    // res.send("up and runnig");
 
 })
 
